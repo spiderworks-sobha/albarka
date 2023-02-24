493ea7d26aacc111bb0a3983ad0a1a202e771cfb
spiderworks-sobha <sobha@spiderworks.in>
2023-02-24 14:49:40 +0530

    client info

--- app/Traits/ClientInfoTrait.php
+++ app/Traits/ClientInfoTrait.php
@@ -70,12 +70,12 @@ trait ClientInfoTrait {
         //we will have two since we are not using 'other' argument yet
         //see if version is before or after the name
         if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-            $version= $matches['version'][0];
+            $version= (!empty($matches['version'][0]))?$matches['version'][0]:0;
         }else {
-            $version= $matches['version'][1];
+            $version= (!empty($matches['version'][1]))?$matches['version'][1]:0;
         }
       }else {
-        $version= $matches['version'][0];
+        $version= (!empty($matches['version'][0]))?$matches['version'][0]:0;
       }
     
       // check if we have a number
